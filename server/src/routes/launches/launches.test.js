ed217f8bab02f99667cfc286eae823d9788d17e4
Amandeep Singh <am418470@dal.ca>
2023-07-01 12:14:31 -0300

    testing api endpoints with supertest error cases

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -23,6 +23,13 @@ describe('Test POST /launches', () => {
     target: 'Kepler-62 f',
   };
 
+  const launchDataWithInvalidDate = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-62 f',
+    launchDate: 'zoot',
+  };
+
   test('It should respond with 201 success', async () => {
     const response = await request(app)
       .post('/launches')
@@ -37,6 +44,26 @@ describe('Test POST /launches', () => {
     expect(response.body).toMatchObject(launchDataWithoutDate);
   });
 
-  test('It should catch missing required properties', () => {});
-  test('It should catch invalid dates', () => {});
+  test('It should catch missing required properties', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(launchDataWithoutDate)
+      .expect('Content-Type', /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: 'Missing required launch property',
+    });
+  });
+  test('It should catch invalid dates', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(launchDataWithInvalidDate)
+      .expect('Content-Type', /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: 'Invalid launch date',
+    });
+  });
 });

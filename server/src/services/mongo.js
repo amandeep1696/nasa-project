034e09f33e4fa52270e33e0ee9160b3cc4484cc2
Amandeep Singh <am418470@dal.ca>
2023-07-15 19:58:21 -0300

    securing secrets

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-  'mongodb+srv://amansin97:hKs6TJgVZ4KqV6uh@nasacluster.pchgrmk.mongodb.net/nasa?retryWrites=true&w=majority';
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');

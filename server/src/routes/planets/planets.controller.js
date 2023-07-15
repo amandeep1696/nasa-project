56a1a611b2934c4f7d382738b60d93acb8324faa
Amandeep Singh <am418470@dal.ca>
2023-07-15 13:53:03 -0300

    finding docs

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,7 +1,7 @@
-const { getAllplanets } = require('../../models/planets.model');
+const { getAllPlanets } = require('../../models/planets.model');
 
 async function httpGetAllPlanets(req, res) {
-  return res.status(200).json(await getAllplanets());
+  return res.status(200).json(await getAllPlanets());
 }
 
 module.exports = {
